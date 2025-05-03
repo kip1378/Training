cb8efe97d7189ab14ca26d6ac6923289eb832f76
kip1378 <kip1378@gmail.com>
2025-05-02 21:18:57 -0700

    training

--- src/LeetCode/HouseRobber.java
+++ src/LeetCode/HouseRobber.java
@@ -1,4 +1,22 @@
 package LeetCode;
 
 public class HouseRobber {
+    public int rob(int[] nums) {
+
+        if (nums.length == 0) {return  0;}
+        if(nums.length == 1) {return nums[0];}
+        if (nums.length == 2) {return Math.max(nums[0], nums[1]);}
+
+        int prev1 = nums[1]; // i=  1
+        int prev2 = nums[0]; // i = 0
+        int cur = 0;
+        for (int i = 2; i < nums.length; i++) {
+            cur = Math.max(prev1, prev2 + nums[i]); // i = 2
+
+           prev2 = prev1;
+           prev1 = cur;
+        }
+
+        return cur;
+    }
 }
